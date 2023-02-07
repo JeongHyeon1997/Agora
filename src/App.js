a4f22947e71c980e1d7e9bf365df91ee7cb39858
JeongHyeon1997 <maszl951@naver.com>
2023-02-07 15:24:56 +0900

    Agora Settings

--- src/App.js
+++ src/App.js
@@ -11,30 +11,31 @@ const App = () => {
   const [rtcProps, setRtcProps] = useState({
     appId: "4f3102bc6420468ab4df8406232cda1b",
     channel: "", // your agora channel
-    token:
-      "007eJxTYNiy6avDmpeHwyZueHix7U2d2owrvA/6bua5GkS9NTaJ/1OvwGCSZmxoYJSUbGZiZGBiZpGYZJKSZmFiYGZkbJSckmiYxLH5YXJDICNDwDprBkYoBPFZGEpSi0sYGAA5GCHa",
+    token: "",
   });
-  const [uId, setuId] = useState(0);
-
-  const uidUp = () => {
-    setuId(uId + 1);
-    console.log(uId);
-  };
 
   const changeChannel = (e) => {
     setRtcProps({ ...rtcProps, channel: e.target.value });
-    console.log(rtcProps.channel);
   };
   const callbacks = {
     EndCall: () => setVideoCall(false),
   };
-  useEffect(() => {
+  const showVideo = async () => {
     api.get(`/rtc/${rtcProps.channel}/1/uid/0/?expiry=60`).then((res) => {
-      console.log(res);
-      console.log(rtcProps);
       setRtcProps({ ...rtcProps, token: res.data.rtcToken });
+      setVideoCall(true);
     });
-  }, [videoCall]);
+  };
+
+  useEffect(() => {
+    console.log(
+      "--------------------------------------------------------------"
+    );
+    console.log(rtcProps);
+    console.log(
+      "--------------------------------------------------------------"
+    );
+  }, [rtcProps]);
 
   return videoCall ? (
     <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
@@ -43,8 +44,13 @@ const App = () => {
   ) : (
     <>
       <input onChange={(e) => changeChannel(e)} />
-      <h3 onClick={() => setVideoCall(true)}>Start Call</h3>
-      <button onClick={uidUp} />
+      <h3
+        onClick={() => {
+          showVideo();
+        }}
+      >
+        Start Call
+      </h3>
     </>
   );
 };
