04d0dbdb84868a058843357919bef95f9a4d328b
JeongHyeon1997 <maszl951@naver.com>
2023-02-13 21:28:31 +0900

    Camera & Audio Settings

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,32 +1,69 @@
 import { useEffect, useRef, useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import useNavigate from "../hooks/useNavigate";
 
 const Test = () => {
   const { goHome } = useNavigate();
-  const [play, setPlay] = useState(false);
-  const videoRef = useRef();
+  const [videoPlay, setVideoPlay] = useState(false);
+  const [audioPlay, setAudioPlay] = useState(false);
+  const Ref = useRef();
 
   useEffect(() => {
     startVideo();
+    startAudio();
   }, []);
 
+  // 카메라 On / Off
   const startVideo = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-    if (!play) {
-      if (videoRef && videoRef.current && !videoRef.current.srcObject) {
-        videoRef.current.srcObject = stream;
-      }
-      setPlay(true);
+    // 카메라 권한
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+    });
+    if (!videoPlay) {
+      setVideoPlay(true);
+      // 카메라 재생
+      Ref.current.srcObject = stream;
+    } else if (videoPlay) {
+      setVideoPlay(false);
+      Ref.current.srcObject.getTracks().forEach((track) => {
+        console.log(track);
+        // 카메라 정지
+        track.stop();
+      });
     }
   };
+
+  const startAudio = async () => {
+    const audio = document.querySelector("audio");
+    // 마이크 권한
+    const mike = await navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((res) => {
+        console.log(res);
+        if (!audioPlay) {
+          setAudioPlay(true);
+          audio.srcObject = res;
+          audio.onloadedmetadata = () => {
+            audio.play();
+          };
+        } else if (audioPlay) {
+          setAudioPlay(false);
+          audio.srcObject = res;
+          audio.onloadedmetadata = () => {
+            audio.pause();
+          };
+        }
+      });
+  };
+
   return (
     <>
       <Body>
         <Start>여기는 내장 카메라로 띄운 화면 입니당</Start>
-        <Advertising> 무려 광고 영역 ? </Advertising>
-        <video autoPlay ref={videoRef} className="videoSection" width={"50%"} />
-        <Advertising> 무려 광고 영역 ? </Advertising>
+        <video autoPlay ref={Ref} className="videoSection" width={"50%"} />
+        <audio />
+        <Button onClick={startVideo}>카메라 On / Off</Button>
+        <Button onClick={startAudio}>마이크 On / Off</Button>
       </Body>
       <Button onClick={goHome}>홈으로 돌아가기</Button>
     </>
@@ -36,7 +73,15 @@ const Test = () => {
 export default Test;
 
 const Button = styled.button`
-  width: 100%;
+  width: 50%;
+  border: solid 1px black;
+  margin-top: 10pt;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  color: white;
+  background-color : black;
+}
 `;
 
 const Body = styled.div`
@@ -49,11 +94,4 @@ const Start = styled.div`
   margin-left: auto;
   margin-right: auto;
   margin-top: 20pt;
-`;
-
-const Advertising = styled.div`
-  display: inline-block;
-  text-align: center;
-  border: solid 5px red;
-  width: 23%;
 `;
